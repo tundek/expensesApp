c25e0a205c84b05348c3cf5b23a06a92375be3eb
tundek <tundek79@gmail.com>
2018-05-24 14:04:39 +0100

    git status

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
-function() {
-	alert('Helo World');
-}
+
+var app = angular.module('myApp', [])
+
+app.controller('mainController', ['$scope',function($scope) {
+	$scope.title = "Welcome to my Sample App";
+
+	$scope.expense = {
+		description: 'Clothes',
+		amount: 5
+	}
+
+	$scope.increaseAmount = function() {
+		$scope.expense.amount++;
+	}
+}]);
